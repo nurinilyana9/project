fb9ce45c7fc5a25cc71370baea750eced29e064b
nurinilyana9 <80583814+nurinilyana9@users.noreply.github.com>
2021-03-13 21:08:58 +0800

    Add files via upload

---
+++ COD.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+if(!isset($_SESSION['login_user2'])){
+header("location: customerlogin.php"); 
+}
+
+unset($_SESSION["cart"]);
+?>
+
+<html>
+
+  <head>
+    <title> Cart | Le Cafe' </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/COD.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container"></div>
+    </nav>
+
+
+
+        <div class="container">
+          <div class="jumbotron">
+            <h1 class="text-center" style="color: green;"><span class="glyphicon glyphicon-ok-circle"></span> Order Placed Successfully.</h1>
+          </div>
+        </div>
+        <br>
+
+<h2 class="text-center"> Thank you for Ordering'! We will update your order via email.</h2>
+
+<?php 
+  $num1 = rand(100000,999999); 
+  $num2 = rand(100000,999999); 
+  $num3 = rand(100000,999999);
+  $number = $num1.$num2.$num3;
+?>
+
+<h3 class="text-center"> <strong>Your Order Number:</strong> <span style="color: blue;"><?php echo "$number"; ?></span> </h3>
+	  
+<div class="col-md-6 col-sm-6 col-xs-6 pad-adjust">
+                              <a href="index.php"><input type="submit" class="btn btn-success btn-block" value="Home" required="" /></a>  
+                            </div>
+        </body>
+
+</html>
